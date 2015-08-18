4e4c3963906ff14987a704db373b40fa62e62629
dong yoon kang <yoon@dongs-MacBook-Pro.local>
2015-08-17 22:16:24 -0700

    added schema and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
+  has_many :pets
+
   before_save -> do
     self.uid = SecureRandom.uuid
     skip_confirmation!
--- db/migrate/20150818050852_create_pets.rb
+++ db/migrate/20150818050852_create_pets.rb
@@ -1,7 +1,8 @@
 class CreatePets < ActiveRecord::Migration
   def change
     create_table :pets do |t|
-
+      t.string :name
+      t.belongs_to :user
       t.timestamps
     end
   end
