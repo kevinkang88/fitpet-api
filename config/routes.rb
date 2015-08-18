673fdaf8e94d7e5d68571b29ee8c91909bc2656d
dong yoon kang <yoon@dongs-MacBook-Pro.local>
2015-08-17 21:58:05 -0700

    added routes and updated schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/auth'
+  scope '/api' do
+    mount_devise_token_auth_for 'User', at: '/auth'
+    resources :pets
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
